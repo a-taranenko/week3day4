daa9d9b3cfda96bda09e71c2a2d7d0521f996174
Anton Taranenko <taranenko.anton@gmail.com>
2016-09-22 19:47:57 +0000

    Adds the closures exercise

---
+++ closures.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// for (var i = 0; i < 3; i++) {
+//   setTimeout((function (counter) {
+//     return function() { console.log(counter); };
+//     })(i), 1000 + i);
+//   // myTimer(i);
+// }
+
+
+
+// function myTimer(counter) {
+//   setTimeout(function() { console.log(counter); }, 1000 + counter);
+// }
+
+for (var i = 0; i < 3; i++) {
+  setTimeout(myFunc(i), 1000 + i);
+}
+
+function myFunc(counter) {
+  return function() { console.log(counter); };
+}
